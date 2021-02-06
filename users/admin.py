501e1ca7c78daeb3cb0a05306ee8130e9a4cf9f4
netsus <cwjcsk@naver.com>
2021-02-06 20:17:29 +0900

    #3.4 Falling in Love with Admin Panel admin.py 수정: djnago administration 페이지 수정 1. Decorator  데코레이터는 클래스바로 위에 있어야 작동한다. ex) customUserAdmin 클래스를 이용해서, models.User를 컨트롤한다.   @admin.register(models.User)   class CustomUserAdmin(admin.ModelAdmin): 2. list_display : Admin 페이지에 보여줄 정보 선택 3. list_filter : Admin 페이지에 보여줄 정보에서 값 선택하여 보는 기능

--- users/admin.py
+++ users/admin.py
@@ -4,4 +4,11 @@ from . import models
 # Register your models here.
 @admin.register(models.User)
 class CustomUserAdmin(admin.ModelAdmin):
-    pass
+    """ Custom User Admin """
+
+    list_display = ("username", "email", "gender", "language", "currency", "superhost")
+    list_filter = (
+        "language",
+        "currency",
+        "superhost",
+    )
