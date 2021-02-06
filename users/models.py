5070ca453a7a320c11b694ac14f9a06b99195a8a
netsus <cwjcsk@naver.com>
2021-02-06 19:47:03 +0900

    #3.3 Finishing User Model 1. CHOICES에 들어가는 튜플   (DB로갈 값, 사용자에게 보여질 값) 2. models Filed 5가지 ImageField, CharField, TextField, DateField, BooleanField default=False하면, blank=True안해도 된다. null=True : db에서 값이 없어도 된다. blank=True : 사용자가 값을 입력하지 않아도 된다. (is not Required)

--- users/models.py
+++ users/models.py
@@ -16,8 +16,27 @@ class User(AbstractUser):
         (GENDER_OTHER, "Other"),
     )
 
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_KOREAN, "Korean"),
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOCES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
+
     avatar = models.ImageField(null=True, blank=True)
     gender = models.CharField(
         choices=GENDER_CHOICES, max_length=10, null=True, blank=True
     )
     bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(null=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
+    currency = models.CharField(
+        choices=CURRENCY_CHOCES, max_length=3, null=True, blank=True
+    )
+    superhost = models.BooleanField(default=False)
