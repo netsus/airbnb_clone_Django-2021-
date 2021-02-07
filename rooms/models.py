54a23b6da76276c0ef8f5d23078c5e6ea9d327ae
netsus <cwjcsk@naver.com>
2021-02-07 18:27:02 +0900

    #4.3 Many-to-Many relation Room과 RoomType은 다-대-다 관계. 1. python 동작 원리: class -> string 모든 class들이 가지고 있는 하나의 method가 바로 string method(__str__) rooms - models.py에 아래 코드 추가하면, 방이름을 목록에서  볼 수 있다. def __str__(self):     return self.name 2. rooms - models.py   1) AbstractItem class     카테고리만 다를 뿐 카테고리마다 item이 들어가기 때문에 그 이름이 제목으로 출력되도록 abstract=true이고, def__str__(self) return self.name 인 class를 생성함.   2) RoomType class     -> room_type = models.ManyToManyField(RoomType, blank=True)     Room과 RoomType을 다대다관계로 사용.

--- rooms/models.py
+++ rooms/models.py
@@ -4,6 +4,24 @@ from core import models as core_models
 from users import models as user_models  # users폴더의 models.py -> user_models
 
 
+class AbstractItem(core_models.TimeStampedModel):
+
+    """ Abstract Item """
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+
+    pass
+
+
 class Room(core_models.TimeStampedModel):
 
     """ Room Model Definition"""
@@ -24,3 +42,7 @@ class Room(core_models.TimeStampedModel):
     host = models.ForeignKey(
         user_models.User, on_delete=models.CASCADE
     )  # user_models.User 모델을 참조
+    room_type = models.ManyToManyField(RoomType, blank=True)
+
+    def __str__(self):
+        return self.name
