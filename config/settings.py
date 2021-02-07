4d33574386abfef4d3cb931a564a60daaadc6cc6
netsus <cwjcsk@naver.com>
2021-02-07 17:29:28 +0900

    #4.1 Room Model part 1 1. core - models.py 수정   1) pipenv install django-countries -> settings.py - THIRD_PARTY_APPS = [django_countries]   2) Date(Time)Field     auto_now_add = True : Model이 생성된 날짜     auto_now = True : Model이 업데이트된 날짜
    2. rooms - models.py
    서로 다른 application간 연결 (ForeignKey)
      from users import models as user_models # users폴더의 models.py -> user_models
      host = models.ForeignKey(user_models.User, on_delete=models.CASCADE) # user_models.User
    
    *tip: 패키지 import 순서 : python 패키지 -> django 패키지 -> third-party 패키지 -> 내가만든 패키지

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,9 @@ DJANGO_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
-THIRD_PARTY_APPS = []
+
+THIRD_PARTY_APPS = ["django_countries"]
+
 PROJECT_APPS = [
     "core.apps.CoreConfig",
     "users.apps.UsersConfig",
