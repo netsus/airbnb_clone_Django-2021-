2c6eb07798acfa4faada78a7a85c7734b6edc622
netsus <cwjcsk@naver.com>
2021-02-07 19:44:31 +0900

    #4.5 Meta Class and Photos Model 1. verbose_name   1) verbose_name_plural은 복수형으로 보일 이름   Meta class는 모든 모델클래스에 대해서, 정보를 표시해주는 클래스. Meta class 내부에서 verbose_name_plural을 설정해주면, 해당 모델의 표시되는 이름을 설정할 수 있다. ex) Amenity 모델의 표시되는 이름을 Amenities로 설정 class Amenity(AbstractItem):
        ''' Amenity Model Definition '''
    
        class Meta:
            verbose_name_plural = 'Amenities'
      2) verbose_name
        단수형이름 수정 -> 표시는 복수형으로 됨.
    ex) 단수형 이름 House Rule로 줌 -> House Rules로 표시됨(복수형)
    class HouseRule(AbstractItem):
    
        ''' HouseRule Model Definition '''
    
        class Meta:
            verbose_name = 'House Rule'
    2. models 가져올때, 쌍따옴표로 클래스 가져오기
    ex) Use before Reference 가능. users폴더의 User 클래스 편하게 가져오기 가능.
    room = models.ForeignKey('Room', on_delete=models.CASCADE )
    host = models.ForeignKey( 'users.User', on_delete=models.CASCADE )

--- rooms/admin.py
+++ rooms/admin.py
@@ -4,10 +4,22 @@ from . import models
 
 @admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
 class ItemAdmin(admin.ModelAdmin):
+
+    """ Item Admin Definition """
+
     pass
 
 
 @admin.register(models.Room)
 class RoomdAdmin(admin.ModelAdmin):
+
+    """ Room Admin Definition """
+
+    pass
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    """ """
 
     pass
