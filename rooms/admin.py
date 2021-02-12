d083923f68312abf93d26b04d7765ec725887751
netsus <cwjcsk@naver.com>
2021-02-12 23:13:19 +0900

    #8.5 Photo Admin
    rooms - admin.py :
    from django.utils.html import mark_safe  # python에서 HTML 실행가능.
    ex) url을 통해 이미지 보여주기
    def get_thumbnail(self, obj):
            return mark_safe(f'<img width='150px' src='{obj.file.url}' />')

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 from . import models
 
 
@@ -124,4 +125,9 @@ class PhotoAdmin(admin.ModelAdmin):
 
     """ Photo Admin Definition"""
 
-    pass
+    list_display = ("__str__", "get_thumbnail")
+
+    def get_thumbnail(self, obj):
+        return mark_safe(f'<img width="150px" src="{obj.file.url}" />')
+
+    get_thumbnail.short_description = "Thumbnail"
