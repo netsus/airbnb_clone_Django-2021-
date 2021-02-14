56aae714b416179234f187234d2f63d9657e22d4
netsus <cwjcsk@naver.com>
2021-02-15 01:13:38 +0900

    #9.6 seed_reviews review생성 과정 - management - seed_reviews.py handle 함수 1. seed 생성 seeder = Seed.seeder() 2. seeder.add_entity(모델, 생성할 개수, Custom할 Field)   1) 별점 범위지정 0이상, 6미만 : random.randint(0, 6)   2) ForeignKey 지정 : ex) 'room': lambda x: random.choice(rooms) 3. seeder 실행 seeder.execute() 4. 생성 완료 알림 self.stdout.write(self.style.SUCCESS(f'{number} reviews created!'))

---
+++ reviews/management/commands/seed_reviews.py
@@ -0,0 +1,42 @@
+import random
+from django.core.management.base import BaseCommand
+from django_seed import Seed
+from reviews import models as review_models
+from users import models as user_models
+from rooms import models as room_models
+
+
+class Command(BaseCommand):
+
+    help = "This command creates reviews"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--number",
+            default=1,
+            type=int,
+            help="How many reviews do you want to create",
+        )
+
+    def handle(self, *args, **options):
+
+        number = int(options.get("number"))
+        seeder = Seed.seeder()
+        users = user_models.User.objects.all()
+        rooms = room_models.Room.objects.all()
+        seeder.add_entity(
+            review_models.Review,
+            number,
+            {
+                "accuracy": lambda x: random.randint(0, 6),
+                "communication": lambda x: random.randint(0, 6),
+                "cleanliness": lambda x: random.randint(0, 6),
+                "location": lambda x: random.randint(0, 6),
+                "check_in": lambda x: random.randint(0, 6),
+                "value": lambda x: random.randint(0, 6),
+                "room": lambda x: random.choice(rooms),  # ForeignKey 추가
+                "user": lambda x: random.choice(users),  # ForeignKey 추가
+            },
+        )
+        seeder.execute()
+        self.stdout.write(self.style.SUCCESS(f"{number} reviews created!"))
