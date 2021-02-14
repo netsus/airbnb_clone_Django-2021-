6eab9fec2407d38a2057040a8c8dc31b07f0d660
netsus <cwjcsk@naver.com>
2021-02-14 23:44:54 +0900

    #9.4 seed_rooms part 2 from django.contrib.admin.utils import flatten -> flatten은 여러차수의 배열을 1차 배열(단일 리스트)로 바꿔주는 함수
    created_photos = seeder.execute()  # 생성된 방의 Primary Key return
    created_clean = flatten(created_photos.values()) # flatten 함수 사용
    for pk in created_clean:
        room = room_models.Room.objects.get(pk=pk)
        for i in range(3, random.randint(10, 17)):   # Photo 3개 이상, 10~17개 이하로 생성
            room_models.Photo.objects.create(
                caption=seeder.faker.sentence(),     # 캡션은 seeder.faker.sentence() 이용
                room=room,                           # room은 Room Object
                file=f'room_photos/{random.randint(1,31)}.webp',   #  room_photos 디렉토리에서 사진 찾음
            )

---
+++ rooms/management/commands/seed_rooms.py
@@ -0,0 +1,51 @@
+import random
+from django.core.management.base import BaseCommand
+from django.contrib.admin.utils import flatten
+from django_seed import Seed
+from rooms import models as room_models
+from users import models as user_models
+
+
+class Command(BaseCommand):
+
+    help = "This command creates many rooms"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--number",
+            default=1,
+            type=int,
+            help="How many rooms do you wnat to create?",
+        )
+
+    def handle(self, *args, **options):
+        number = int(options.get("number"))
+        seeder = Seed.seeder()
+        all_users = user_models.User.objects.all()
+        room_types = room_models.RoomType.objects.all()
+        seeder.add_entity(
+            room_models.Room,
+            number,
+            {
+                "name": lambda x: seeder.faker.address(),
+                "host": lambda x: random.choice(all_users),
+                "room_type": lambda x: random.choice(room_types),
+                "guests": lambda x: random.randint(1, 20),
+                "price": lambda x: random.randint(1000, 700000),
+                "beds": lambda x: random.randint(1, 3),
+                "bedrooms": lambda x: random.randint(1, 5),
+                "baths": lambda x: random.randint(1, 3),
+            },
+        )
+        created_photos = seeder.execute()  # 몇번 방인지 Primary Key return
+        created_clean = flatten(created_photos.values())
+        for pk in created_clean:
+            room = room_models.Room.objects.get(pk=pk)
+            for i in range(3, random.randint(10, 16)):
+                room_models.Photo.objects.create(
+                    caption=seeder.faker.sentence(),
+                    room=room,
+                    file=f"room_photos/{random.randint(1,31)}.webp",
+                )
+
+        self.stdout.write(self.style.SUCCESS(f"{number} rooms created!"))
