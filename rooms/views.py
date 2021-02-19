6901d60c30510f27d1686ade1c1c1e4fa2662bb9
netsus <cwjcsk@naver.com>
2021-02-20 00:35:08 +0900

    #11.2 Pages List Navigation rooms-home.html에 페이지 출력 1. 전체페이지중 현재 페이지 출력   1) 전체 페이지     page_count = ceil(models.Room.objects.count() / page_size) 2. 페이지 선택하여 이동 기능 추가   1) 장고 html에서 python range 문법 사용 안됨 -> views.py에서 context로 range 전달 ex) context로 range 전달 -> page_range 사용가능     context={'page_range': range(1, page_count + 1)} # views.py에서 range 전달     {% for page in page_range %} # 전달받은 range 사용         <a href='?page={{page}}'>{{page}}</a>     {% endfor %}

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,4 @@
+from math import ceil
 from django.shortcuts import render
 from . import models
 
@@ -8,4 +9,14 @@ def all_rooms(request):
     limit = page_size * page
     offset = limit - page_size
     all_rooms = models.Room.objects.all()[offset:limit]
-    return render(request, "rooms/home.html", context={"rooms": all_rooms})
+    page_count = ceil(models.Room.objects.count() / page_size)
+    return render(
+        request,
+        "rooms/home.html",
+        context={
+            "rooms": all_rooms,
+            "page": page,
+            "page_count": page_count,
+            "page_range": range(1, page_count + 1),
+        },
+    )
