358c49b232b5ad702809c8ba1859f61f66223136
netsus <cwjcsk@naver.com>
2021-02-20 00:15:01 +0900

    #11.1 Pagination with Limit and Offset 1. Convention : URL뒤에 붙는 것 1) 기초 예시 ex) http://127.0.0.1:8000/\?page\=2 해당페이지로 들어가면 request.GET인자로 컨벤션이 들어간다 print(request.GET ) -><QueryDict: {'page': ['2']}> ex2) 여러 인자, 여러 값 http://127.0.0.1:8000/\?page\=2\&city\=seoul,busan print(request.GET ) -><QueryDict: {'page': ['2'], 'city': ['seoul,busan']}>
    2)  request.GET (QueryDictionary)
    일반 딕셔너리처럼 .get 메소드쓸 수 있다. (다른 메소드도 사용가능)
    ex1) print(request.GET.get('page', 0))
    -> page인자 있을땐 해당 페이지출력, 페이지 인자 없으면 0 출력
    
    2. Pagination
    [offset: limit]으로 objects.all()을 슬라이싱
    10페이지씩 출력

--- rooms/views.py
+++ rooms/views.py
@@ -3,5 +3,9 @@ from . import models
 
 
 def all_rooms(request):
-    all_rooms = models.Room.objects.all()
+    page = int(request.GET.get("page", 1))
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    all_rooms = models.Room.objects.all()[offset:limit]
     return render(request, "rooms/home.html", context={"rooms": all_rooms})
