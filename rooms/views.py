651fe85f001b185942260a2deaf1a1e70d256988
netsus <cwjcsk@naver.com>
2021-02-20 18:43:14 +0900

    #11.6 Handling Exceptions 1. 장고에서 예외다루기 페이지에 나오는 예외를 체크해서 except <해당에러>:로 예외처리를 해주면 어떤 에외가 왜 발생했는지 추적이 더 쉽다. ex) EmpthPage 예외 from django.core.paginator import EmptyPage # 예외 Import try:     ~Something~ except EmptyPage:  # EmptyPage 예외발생하면 / URL로 가도록 하기     return redirect('/')

--- rooms/views.py
+++ rooms/views.py
@@ -1,17 +1,14 @@
-from math import ceil
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from . import models
 
 
 def all_rooms(request):
-    page = int(request.GET.get("page"))
+    page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
     paginator = Paginator(room_list, 10, orphans=6)
-    rooms = paginator.get_page(page)  # rooms는 <class 'django.core.paginator.Page'>
-    print(type(rooms))
-    return render(
-        request,
-        "rooms/home.html",
-        context={"page": rooms},
-    )
+    try:
+        rooms = paginator.page(int(page))  # rooms는 <class 'django.core.paginator.Page'>
+        return render(request, "rooms/home.html", context={"page": rooms})
+    except EmptyPage:
+        return redirect("/")
