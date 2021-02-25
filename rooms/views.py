c6c2a4e140f9ca4bc0daab423986d326e2a40251
netsus <cwjcsk@naver.com>
2021-02-26 01:08:47 +0900

    #13.2 Select the Form 1. rooms/views.py render의 context변수 form과 choices로 나눔 unpacking =>**을 사용하여 dictionary 언패킹 ex)  {**form, **choices}
    2. search.html
    선택하는 option 태그에서 선택한 뒤에 새로고침하거나 제출해도 선택된거 유지하게하기 -> selected 속성 이용
    ex 1) room_type에서 0번쨰 pk가 room_type이면 Any kind 선택 유지하게하기
    <option value='0' {% if s_room_type == 0 %}selected{% endif %}>Any kind</option>
    ex 2) country.code가 s_country (s_country = request.GET.get('country', 'KR') - 페이지에서 선택된 country)와 같으면 선택 유지하게하기
    <option value='{{country.code}}' {% if country.code == s_country %}selected{% endif %}> {{country.name}} </option>

--- rooms/views.py
+++ rooms/views.py
@@ -26,9 +26,23 @@ def search(request):
     print(request.GET)
     city = request.GET.get("city", "Anywhere")
     city = str.capitalize(city)
+    country = request.GET.get("country", "KR")
+    room_type = int(request.GET.get("room_type", 0))
     room_types = models.RoomType.objects.all()
+
+    form = {
+        "city": city,
+        "s_country": country,
+        "s_room_type": room_type,
+    }
+
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+    }
+
     return render(
         request,
         "rooms/search.html",
-        {"city": city, "countries": countries, "room_types": room_types},
+        {**form, **choices},
     )
