ef8063f9b084fc87d6a3a9ef356650ec9e666186
netsus <cwjcsk@naver.com>
2021-02-27 19:42:43 +0900

    #13.10 Finishing Up! (Django Form) 1. views.py 에 paginator 생성.   paginator는 어떤 순서로 정렬되어 있어야함. -> queryset에  .order_by()를 줘야함.   ex) orderby한 결과를 queryset에 넣고 -> Paginator 생성 -> rooms에 넣기                 qs = models.Room.objects.filter(**filter_args).order_by('-created')                 paginator = Paginator(qs, 3, orphans=1)                 page = request.GET.get('page', 1)                 rooms = paginator.get_page(page) 2. Filtering과 Pagination 동시에 하기 링크 : https://www.caktusgroup.com/blog/2018/10/18/filtering-and-pagination-django/   1) 컨셉: URL에서 page 파라미터만 변경가능하도록 파이썬 파일과 함수를 만들기 -? HTML에서 해당 파이썬 파일 load 해서 파라미터만 변경하여 사용   1) rooms/templatetags/my_tags.py 생성     -> template에 등록, param이 존재하면 대체하고, 없으면 추가하는 함수 작성.     register = template.Library()
        @register.simple_tag(takes_context=True)
        def param_replace(context, **kwargs):
            d = context['request'].GET.copy()
            for k, v in kwargs.items():
                d[k] = v
            for k in [k for k, v in d.items() if not v]:
                del d[k]
            return d.urlencode()
    
      2) rooms/search.html에 param_replace 사용
      -> 이전 페이지가 존재하면, url에 page=1 추가, page 존재하면 page=1로 대체
        {% load my_tags %}  // 위에서 생성한 my_tags load
            {% if rooms.has_previous %}
                <a href='?{% param_replace page=1 %}'>First</a>

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import ListView, DetailView, View
 from django.shortcuts import render, redirect
-from django_countries import countries
+from django.core.paginator import Paginator
 from . import models, forms
 
 
@@ -81,13 +81,23 @@ class SearchView(View):
                 for facility in facilities:
                     filter_args["facilities"] = facility
 
-                rooms = models.Room.objects.filter(**filter_args)
+                qs = models.Room.objects.filter(**filter_args).order_by("-created")
+
+                paginator = Paginator(qs, 1, orphans=0)
+
+                page = request.GET.get("page", 1)
+
+                rooms = paginator.get_page(page)
+                return render(
+                    request,
+                    "rooms/search.html",
+                    {"form": form, "rooms": rooms},
+                )
 
         else:
             form = forms.SearchForm()
-
         return render(
             request,
             "rooms/search.html",
-            {"form": form, "rooms": rooms},
+            {"form": form},
         )
