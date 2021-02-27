263214afe8521b8047e559221399788c16a63a3b
netsus <cwjcsk@naver.com>
2021-02-28 00:28:28 +0900

    # 14.4 Log in and Log Out 로그인 확인 방법: 사용자 화면에서 로그인했을때, admin 계정이라면, admin에서도 로그인 되는걸로 확인가능. 1. 로그인 관련 import   1) from django.contrib.auth import authenticate, login, logout   2) Log in 방법     authenticate 함수로 request와 username(ID), password를 입력받고, 그 값이 None이 아니면 로그인하기         user = authenticate(request, username=email, password=password)             if user is not None:                 login(request, user)                 return redirect(reverse("core:home"))  # 로그인 성공하면 홈으로 가기   3) Context Processor와 Log out     Context Processor - 특정 context는 장고가 자체적으로 모든 HTML에 제공. (auth에 user 포함 -> user 사용가능)     ex) user가 권한 있을때. 즉, 로그인 되어있을 때 Log out 누르면 users : logout 실행 (users 앱에서 logout 함수 실행)         {% if user.is_authenticated %}             <li><a href="{% url "users:logout" %}">Log out</a></li>
      users/views.py에 logout 함수 따로 만들고, urls.py에 등록
        def log_out(request):
            logout(request)
            return redirect(reverse("core:home"))
        --> urls.py의 urlpatterns에 path("logout", views.log_out, name="logout"), 추가

--- users/urls.py
+++ users/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 
 app_name = "users"
-urlpatterns = [path("login", views.LoginView.as_view(), name="login")]
+urlpatterns = [
+    path("login", views.LoginView.as_view(), name="login"),
+    path("logout", views.log_out, name="logout"),
+]
