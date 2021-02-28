c2e0ee0c66fff9b729831607fb1cd73c9e91ddc4
netsus <cwjcsk@naver.com>
2021-03-01 02:10:46 +0900

    # 18.0 Getting Authorization Code Kakao Developer에서 애플리케이션 추가하기 -> REST_API 키 사용 (client_id나 키값으로) 카카오 로그인에서 활성화 설정 -> 상태 On으로 변경 -> Redirect URI에 카카오로 로그인할 우리 주소 작성 --> "http://127.0.0.1:8000/users/login/kakao/callback" 등록 -> 동의항목에서 필요한 정보 동의 등록하기 - 필수 혹은 선택으로
    users/views.py
    1. kakao_login 함수
    --> client_id로 .env에 카카오 REST_API키를 저장한거 불러옴
    --> return redirect("카카오 oauth URL/?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code")
        --> 즉, (카카오 OAUTH URL)/(필요 전달값)
    2. kakao_callback 함수
    kakao_login함수가 실행되며 리다이렉트된 주소에서 code인자 가져온 후, access_token 얻기
    에러는 모두 except로 처리하여 KakaoException 예외로 모아주기 --> redirect users : login
    
    social_login.html
        <a href="{%url "users:kakao-login" %}">
            Continue with Kakao
        </a>
    
    users/urls.py
        urlpatterns에 아래 변수 추가
        path("login/kakao", views.kakao_login, name="kakao-login"),
        path("login/kakao/callback", views.kakao_callback, name="kakao-callback"),

--- users/views.py
+++ users/views.py
@@ -133,3 +133,26 @@ def github_callback(request):
             raise GithubException()
     except GithubException:
         return redirect(reverse("users:login"))
+
+
+def kakao_login(request):
+    client_id = os.environ.get("KAKAO_ID")
+    redirect_uri = "http://127.0.0.1:8000/users/login/kakao/callback"
+    return redirect(
+        f"https://kauth.kakao.com/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code"
+    )
+
+
+class KakaoException(Exception):
+    pass
+
+
+def kakao_callback(request):
+    try:
+        code = request.GET.get("code")
+        client_id = os.environ.get("KAKAO_ID")
+        token_request = requests.get(
+            f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={client_id}"
+        )
+    except KakaoException:
+        return redirect(reverse("users:login"))
