f2550af401f8c7dd16d2d86b38474adf50c1591d
netsus <cwjcsk@naver.com>
2021-03-01 16:06:30 +0900

    # 18.3 Closing Thoughts users/forms.py SignUpForm에서 forms.ModelForm을 상속받아서 사용했었는데 UserCreationForm을 사용하면 훨씬 간단하게 쓸 수 있을 뿐 아니라, Django의 password validation 기능도 쓸 수 있다.
    1. Django의 Password Validation 기능 -- 아래처럼 쓰면, 내가 만든 Form에 Password Validation기능만 추가해서 쓸 수 있다.
      1) import
        from django.contrib.auth import (
            authenticate, get_user_model, password_validation,
        )
      2) 사용은 UsercreationForm의 clean_new_password2함수 참조

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from . import models
 
 
@@ -20,36 +21,10 @@ class LoginForm(forms.Form):
             self.add_error("email", forms.ValidationError("User does not exist"))
 
 
-class SignUpForm(forms.ModelForm):
+class SignUpForm(UserCreationForm):
+
+    email = forms.EmailField()
+
     class Meta:
         model = models.User
-        fields = (
-            "first_name",
-            "last_name",
-            "email",
-        )
-
-    password = forms.CharField(widget=forms.PasswordInput)
-    password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
-
-    def clean_password1(self):
-        password = self.cleaned_data.get("password")
-        password1 = self.cleaned_data.get("password1")
-
-        if password != password1:
-            raise forms.ValidationError("Password confirmation does not match")
-        else:
-            return password
-
-    def save(self, *args, **kwargs):
-        user = super().save(commit=False)  # Call the real save() method
-        email = self.cleaned_data.get("email")
-        user.username = email
-        password = self.cleaned_data.get("password")
-        # print(password)
-        user.set_password(password)
-        try:
-            models.User.objects.get(email=email)
-            raise forms.ValidationError("User already exists.")
-        except models.User.DoesNotExist:
-            user.save()
+        fields = ("email",)
