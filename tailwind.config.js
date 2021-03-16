369a8721d45d871debf2e65f02c068f31db1e84c
netsus <cwjcsk@naver.com>
2021-03-17 00:58:14 +0900

    #20.4 Room Cards part One 1. tailwind.config.js : tailwind 커스텀해서 쓰기 ex) spacing(길이 단위 확장) theme: {     extend: {       spacing: {         "25vh" : "25vh",         "50vh" : "50vh",         "75vh" : "75vh",       }     } } 2. style 속성에 background-imgae 주기   1) style="backgorund-image:url(이미지주소);"   ex) nav.html에서 div태그에 style 속성을 통해 백그라운드 이미지 주기 <div class="container bg-cover bg-center"     style="background-image:url(https://images.unsplash.com/photo-1556513492-5ecf87648d3a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop);"> bg-cover : 사진이 전체적으로 cover되도록 하는 기능 bg-center : 이미지 센터

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        "25vh" : "25vh",
+        "50vh" : "50vh",
+        "75vh" : "75vh",
+      },
+      borderRadius: {
+        'xl': '1.5rem'
+      }
+    },
   },
   variants: {
     extend: {},
