c5a02956513ba054bd1c741372dfd4a35a90361e
netsus <cwjcsk@naver.com>
2021-03-19 00:30:21 +0900

    #20.6 Room Cards part Two room_card.html에 Superhost랑 city, total_rating 추가 Superhost는 Superhost인 경우만 출력되게 함 --> {% if room.host.superhost %} ... {% endif %}
    * font-aswsome 추가 - <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">

--- rooms/views.py
+++ rooms/views.py
@@ -9,7 +9,7 @@ class HomeView(ListView):
     """ HomeView Definition """
 
     model = models.Room
-    paginate_by = 10
+    paginate_by = 12
     paginate_orphans = 6
     ordering = "id"
     context_object_name = "rooms"
