43041c413937740018801a86633b2470e4c85e0b
netsus <cwjcsk@naver.com>
2021-03-24 00:27:21 +0900

    #20.10 Signup 1. signup.html input태그로 쓰던것을 forms.py의 filed를 변수로 바꿈. forms.py에서 form 필드 가져옴 (views.py를 통해서 Class Based View로 전달됨.) ex)  내용들 {{form.first_name}} {{form.last_name}} {{form.email}}
    2. users/forms.py
    signupform class
      1) fields 설정 -> widgets 변수에 placeholder 지정

--- users/views.py
+++ users/views.py
@@ -14,7 +14,7 @@ class LoginView(FormView):
     template_name = "users/login.html"
     form_class = forms.LoginForm
     success_url = reverse_lazy("core:home")
-    initial = {"email": "cwjcsk@naver.com"}
+    # initial = {"email": "cwjcsk@naver.com"}
 
     def form_valid(self, form):
         email = form.cleaned_data.get("email")
@@ -35,11 +35,11 @@ class SignUpView(FormView):
     template_name = "users/signup.html"
     form_class = forms.SignUpForm
     success_url = reverse_lazy("core:home")
-    initial = {
-        "first_name": "cwj",
-        "last_name": "csk",
-        "email": "cwj@csk.com",
-    }
+    # initial = {
+    #     "first_name": "cwj",
+    #     "last_name": "csk",
+    #     "email": "cwj@csk.com",
+    # }
 
     def form_valid(self, form):
         form.save()  # form에서 clean을 모두 통과하면, 우리가 만든 save 메소드 실행.
