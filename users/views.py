3d58fb25feb6f380a768bc735c134fa7cf412090
netsus <cwjcsk@naver.com>
2021-03-30 21:55:58 +0900

    #21.6 Update Profile 1. users/views.py  1) UpdateView : update가능한 폼 자동생성 --> get_object함수: default로 url에서 pk를 찾아서 해당 객체 반환. pk 말고 다른거 쓰도록 커스터마이즈 가능. ex)  url에서 pk 안가져오고, 로그인한 해당 유저에 대해 반환하도록 get_object함수 변경 def get_object(self, queryset=None):     return self.request.user  2) model, template_name 부여, field에 수정하고자 하는 필드 부여
    2. users/urls.py
     url 추가:  path("update-profile/", views.UpdateProfileView.as_view(), name="update"),
    
    3. templates/users/user_detail.html
    a 태그 href 수정 --> <a href="{% url "users:update" %}" class="btn-link">Edit Profile</a>
    
    4. templates/users/update-profile.html
    login.html 복사 -> Update profile 페이지 작성 auth_form.html에 cta="Update profile" 부여
    --> Edit profile 클릭하면 업데이트 가능한 페이지 나옴.

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 import os
 import requests
 from django.views import View
-from django.views.generic import FormView, DetailView
+from django.views.generic import FormView, DetailView, UpdateView
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
@@ -216,3 +216,22 @@ class UserProfileView(DetailView):
         context = super().get_context_data(**kwargs)
         context["hello"] = "Hello!"
         return context
+
+
+class UpdateProfileView(UpdateView):
+
+    model = models.User
+    template_name = "users/update-profile.html"
+    fields = (
+        "first_name",
+        "last_name",
+        "avatar",
+        "gender",
+        "bio",
+        "birthdate",
+        "language",
+        "currency",
+    )
+
+    def get_object(self, queryset=None):
+        return self.request.user
