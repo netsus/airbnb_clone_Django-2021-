51a93ce44d619d7415ca7bcae84411d8c8061324
netsus <cwjcsk@naver.com>
2021-03-30 23:14:09 +0900

    #21.7 Change Password 1. style.scss textarea 태그 -> UpdateProfile에서 text쓰는 부분 스타일 지정 select 태그 -> 선택 부분 스타일 지정
    2. 패스워드 변경
     1) users/views.py
    --> UpdatePasswordView 생성 => class UpdatePasswordView(PasswordChangeView)
      template_name = "users/update-password.html"
     2) users/urls.py
    -->path 추가
    path("update-password/", views.UpdatePasswordView.as_view(), name="password"),
     3) templates/users/update-password.html
    --> include auth_form.html -> 비밀번호 변경 화면 구성 (이전 비번, 바꿀 비번, 확인) [placeholder없음]
    path("update-password/", views.UpdatePasswordView.as_view(), name="password"),
    
    3. templates/users/update-profile.html
    --> user.login_method가 email 일때만 패스워드 변경 나오게하기.

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from django.views import View
+from django.contrib.auth.views import PasswordChangeView
 from django.views.generic import FormView, DetailView, UpdateView
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
@@ -225,7 +226,6 @@ class UpdateProfileView(UpdateView):
     fields = (
         "first_name",
         "last_name",
-        "avatar",
         "gender",
         "bio",
         "birthdate",
@@ -235,3 +235,14 @@ class UpdateProfileView(UpdateView):
 
     def get_object(self, queryset=None):
         return self.request.user
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        self.object.username = email
+        self.object.save()
+        return super().form_valid(form)
+
+
+class UpdatePasswordView(PasswordChangeView):
+
+    template_name = "users/update-password.html"
