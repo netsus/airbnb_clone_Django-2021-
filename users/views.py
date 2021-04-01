f3716408eb3a971a5ab2d4c1a497be8ed00c0052
netsus <cwjcsk@naver.com>
2021-04-01 22:16:23 +0900

    21.9 Mixins are Awesome 1.users/mixins.py LoggedOutOnlyView 클래스 생성 유저가 로그인 되었을때와 로그아웃되었을 때를 구분하기 위한 클래스. 로그인 되어있다면, Login 페이지를 눌러도 홈으로 오게하는 기능  1) test_func 함수: 유저가 로그인 되었다면 False 리턴 --> handle_no_permission 함수 실행  2) handle_no_permission 함수: 홈으로 리다이렉트
    2. users/views.py
    LoginView에 위에서 생성한 LoggedOutOnlyView를 상속
    -->위에서 설정한 기능 로그인 뷰에 대해 실행됨
     1) 클래스 : class LoginView(mixins.LoggedOutOnlyView, FormView):

--- users/views.py
+++ users/views.py
@@ -8,10 +8,11 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
 from django.core.files.base import ContentFile
 from django.contrib import messages
-from . import forms, models
+from django.contrib.messages.views import SuccessMessageMixin
+from . import forms, models, mixins
 
 
-class LoginView(FormView):
+class LoginView(mixins.LoggedOutOnlyView, FormView):
     template_name = "users/login.html"
     form_class = forms.LoginForm
     success_url = reverse_lazy("core:home")
@@ -219,7 +220,7 @@ class UserProfileView(DetailView):
         return context
 
 
-class UpdateProfileView(UpdateView):
+class UpdateProfileView(SuccessMessageMixin, UpdateView):
 
     model = models.User
     template_name = "users/update-profile.html"
@@ -232,16 +233,11 @@ class UpdateProfileView(UpdateView):
         "language",
         "currency",
     )
+    success_message = "Profile Updated"
 
     def get_object(self, queryset=None):
         return self.request.user
 
-    def form_valid(self, form):
-        email = form.cleaned_data.get("email")
-        self.object.username = email
-        self.object.save()
-        return super().form_valid(form)
-
     def get_form(self, form_class=None):
         form = super().get_form(form_class=form_class)
         for field_name in form.fields.keys():
@@ -251,9 +247,10 @@ class UpdateProfileView(UpdateView):
         return form
 
 
-class UpdatePasswordView(PasswordChangeView):
+class UpdatePasswordView(SuccessMessageMixin, PasswordChangeView):
 
     template_name = "users/update-password.html"
+    success_message = "Password Updated"
 
     def get_form(self, form_class=None):
         form = super().get_form(form_class=form_class)
@@ -262,3 +259,6 @@ class UpdatePasswordView(PasswordChangeView):
                 "placeholder": field_name.capitalize().replace("_", " ")
             }
         return form
+
+    # def get_success_url(self):
+    #     return super().get_success_url()
