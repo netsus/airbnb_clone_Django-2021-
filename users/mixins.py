f76af718bf5bc0650a299e61466c402c996c7fa6
netsus <cwjcsk@naver.com>
2021-04-02 23:59:53 +0900

    21.10 Great User Mixins 1. users/mixins.py LoggedInOnlyView : 로그인 안했을 떄, 로그인이 필요한 url로 이동하면, 로그인 창으로 리다이렉트 EmailLoginOnlyView : 이메일 로그인 안했을 때, 이메일 로그인이 필요한 url로 이동하면, Can"t go there 메시지 출력  1) mixins.py에 클래스 생성     class LoggedInOnlyView(LoginRequiredMixin):     class EmailLoginOnlyView(UserPassesTestMixin):
    2. users/views.py
     1) LoginView 클래스에 get_success_url 추가
     2) UpdateProfileView 클래스에 mixins.LoggedInOnlyView 추가

--- users/mixins.py
+++ users/mixins.py
@@ -1,5 +1,16 @@
+from django.contrib import messages
+from django.urls import reverse_lazy
 from django.shortcuts import redirect, reverse
-from django.contrib.auth.mixins import UserPassesTestMixin
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+
+
+class EmailLoginOnlyView(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.login_method == "email"
+
+    def handle_no_permission(self):
+        messages.error(self.request, "Can't go there")
+        return redirect(reverse("core:home"))
 
 
 class LoggedOutOnlyView(UserPassesTestMixin):
@@ -10,4 +21,10 @@ class LoggedOutOnlyView(UserPassesTestMixin):
         return not self.request.user.is_authenticated
 
     def handle_no_permission(self):
+        messages.error(self.request, "Can't go there")
         return redirect(reverse("core:home"))
+
+
+class LoggedInOnlyView(LoginRequiredMixin):
+
+    login_url = reverse_lazy("users:login")
