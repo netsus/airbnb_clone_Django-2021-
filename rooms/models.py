f8ab0b7ab7f5a931207c03932d3f7636af010e54
netsus <cwjcsk@naver.com>
2021-04-17 18:00:23 +0900

    #24.0 Room Calendar part One 1. hosting 모드가 아닐때 다른 user의 Room에서 Calendar 볼 수 있게 하기  1) cal.py 생성     python 의 calendar library import 및 Calendar 클래스 생성 --> __init__, get_month 메소드 생성  2) rooms/models.py     cal.py import --> from cal import Calendar     get_calendars 함수 생성 : Calendar 클래스 불러와서 get_month 메소드 작동하는지 테스트  3) templates/rooms/room_detail.html     방의 host가 로그인한 user가 아니고, hosting 모드가 아닐때, room.get_calendars를 출력
    ** config/settings.py
    INSTALLED_APPS 변수에 THIRD_PARTY_APPS 추가 (이전에 놓친 버그 수정)

--- rooms/models.py
+++ rooms/models.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django_countries.fields import CountryField
 from core import models as core_models
 from users import models as user_models  # users폴더의 models.py -> user_models
+from cal import Calendar
 
 
 class AbstractItem(core_models.TimeStampedModel):
@@ -116,3 +117,8 @@ class Room(core_models.TimeStampedModel):
         photos = self.photos.all()[1:5]
         # print(photos)
         return photos
+
+    def get_calendars(self):
+        calendar = Calendar(2019, 11)
+        print(calendar.get_month())
+        return False
