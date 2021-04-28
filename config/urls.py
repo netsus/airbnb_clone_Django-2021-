0bc715e7a3147a40765d22acf0a1ad79f6c5f6a4
netsus <cwjcsk@naver.com>
2021-04-29 00:44:02 +0900

    #24.14 Updating a Reservation 1. reservations : is_finished함수(BookedDay 삭제기능), review_form 전달   1) reservations/models.py : is_finished함수에서 예약 완료된 채로 현재 시간이 check_out 시간 지나면 해당 reservation에 따른 BookedDay 삭제하도록 변경 --> 예약 정상 종료되면 BookedDay 삭제하기 위해서   2) reservations/views.py  :  ReservationDetailView클래스에서 review_form 전달
    2. reviews: 리뷰폼 생성
      1) reviews/forms.py : CreateReviewForm(forms.ModelForm) 생성 - class Meta에 model이랑 fields 지정
      2) reviews/urls.py : CreateReviewForm url 연결
      3) reviews/views.py : create_review 함수 생성 - pass
    
    3. templates/reservations/detail.html : 예약과 리뷰작성 조건문 조절
    -> reservation이 canceled 상태가 아닐때
        1) reservation이 예약확정이고, 동시에 체크아웃 이후인 경우. (confirmed and is_finished) --> Review 작성 가능
        2) else
          2-1) reservation이 pending일 때 : Cancel 가능
          --> user가 reservation.room.host 일 때 : Confrim 가능
    
    config/urls.py : reviews/urls.py 등록

--- config/urls.py
+++ config/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("rooms/", include("rooms.urls", namespace="rooms")),
     path("users/", include("users.urls", namespace="users")),
     path("reservations/", include("reservations.urls", namespace="reservations")),
+    path("reviews/", include("reviews.urls", namespace="reviews")),
     path("admin/", admin.site.urls),  # 장고의 admin 페이지 등록
 ]
 
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def create_review(requst, room):
+    pass
