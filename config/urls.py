73e7a2c1e4ce799f2e5906c4b24fa5f238fd5de8
netsus <cwjcsk@naver.com>
2021-05-11 01:07:39 +0900

    #25.4 Saving Fav Rooms
    1. lists에 view 생성 및 url 생성
      1) config/urls.py ; lists/ url 추가
      2) lists/views.py : save_room 함수 생성 -- 로그인한 유저의 하나 뿐인 찜목록에 방 추가하는 함수
        --> room에 대해 get_or_none으로 방이 있는 경우만 가져온 뒤에, get_or_create로 찜목록(the_list 변수) 생성 및 add -> the_list.rooms.add(room)
        * ManytoMany는 save를  call할 필요가 없다.
      3) lists/urls.py : views에서 생성한 save_room함수 url 추가
    
    2. templates/rooms/room_detail.html -- 앞서 만든 찜목록 추가 a태그에 url 태그 추가
      --> <a href="{% url "lists:save-room" room.pk %}" ... }

--- config/urls.py
+++ config/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path("users/", include("users.urls", namespace="users")),
     path("reservations/", include("reservations.urls", namespace="reservations")),
     path("reviews/", include("reviews.urls", namespace="reviews")),
+    path("lists/", include("lists.urls", namespace="lists")),
     path("admin/", admin.site.urls),  # 장고의 admin 페이지 등록
 ]
 
