2faee9372b3187cad3ad34ddfec46a42cfd686ed
netsus <cwjcsk@naver.com>
2021-05-26 23:43:19 +0900

    #26.3 Postgres part One User나 Password같은 정보를 EB의 환경변수로 지정하기 아마존의 Elastic Beanstalk -> 태그에서 변수 설정 --> settings.py에서 아래처럼 사용 DATABASES = {         "default": {             "ENGINE": "django.db.backends.postgresql",             "HOST": os.environ.get("RDS_HOST"),             "NAME": os.environ.get("RDS_NAME"),             "USER": os.environ.get("RDS_USER"),             "PASSWORD": os.environ.get("RDS_PASSWORD"),             "PORT": "5432",         }     }

--- config/settings.py
+++ config/settings.py
@@ -101,8 +101,12 @@ if DEBUG:
 else:
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+            "ENGINE": "django.db.backends.postgresql",
+            "HOST": os.environ.get("RDS_HOST"),
+            "NAME": os.environ.get("RDS_NAME"),
+            "USER": os.environ.get("RDS_USER"),
+            "PASSWORD": os.environ.get("RDS_PASSWORD"),
+            "PORT": "5432",
         }
     }
 
