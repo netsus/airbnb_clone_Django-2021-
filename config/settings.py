3c6b975051bf6dbbcd8182f8108ed443ee3923be
netsus <cwjcsk@naver.com>
2021-05-30 00:07:30 +0900

    modify SECRET_KEY and .ebextensions file name with order

--- config/settings.py
+++ config/settings.py
@@ -19,9 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    "DJANGO_SECRET"
-)  # i&3vxvw60a5s=6v870*icj^q_o=2--)pyx_v6$*!kdgiascn7j"
+SECRET_KEY = os.environ.get("DJANGO_SECRET", "NTfF6fEHnYxP6HJxK6M")
+# i&3vxvw60a5s=6v870*icj^q_o=2--)pyx_v6$*!kdgiascn7j"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True  # bool(os.environ.get("DEBUG"))
